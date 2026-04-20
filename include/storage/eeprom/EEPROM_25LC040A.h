d0166b3356c5fffd099811454c3f8db8f5c2dca5
Vasilios Tsapalos <v.tsapalos@kaizengaming.com>
2026-04-20 19:52:13 +0300

    Improve EEPROM mechanism

--- include/storage/eeprom/EEPROM_25LC040A.h
+++ include/storage/eeprom/EEPROM_25LC040A.h
@@ -1,18 +1,3 @@
-/*
-Pins:
-1 (CS) -> D10
-2 (SO) -> D12
-3 (WP) -> 5V
-4 (VSS) -> GND
-5 (SI) -> D11
-6 (SCK) -> D13
-7 (HOLD) -> 5V
-8 (VCC) -> 5V
-
-Almost identical with this wiring (https://wolles-elektronikkiste.de/wp-content/uploads/2022/03/Wiring_25LC512_Arduino_Nano.png)
-apart from WP(D9), HOLD(5V) pins.
-*/
-
 #pragma once
 
 #include <Arduino.h>
@@ -26,6 +11,11 @@ public:
 
     void readBytes(uint16_t addr, void *data, uint16_t len);
     void writeBytes(uint16_t addr, const void *data, uint16_t len);
+
+    void writeByte(uint16_t addr, uint8_t data);
+    uint8_t readByte(uint16_t addr);
+
+    void factoryReset();
 
 private:
     uint8_t _cs;
